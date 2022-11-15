5066be8a52f5eee82266da565b3662af3cbae79b
Kyle Kim <kyle.kim@newwavelending.com>
2022-11-15 11:33:55 -0800

    change basepath

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const nextConfig = {
     loader: "akamai",
     path: "",
   },
-  // basePath: "/kyle-kim-portfolio",
-  // assetPrefix: "/kyle-kim-portfolio",
+  basePath: "./",
+  assetPrefix: "./",
 }
 
 module.exports = nextConfig
