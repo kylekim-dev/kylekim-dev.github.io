97afc4ae28bb2a8bc01dfa4fd027718a478993e9
kylekim-dev <kyle.kim0923@gmail.com>
2022-11-23 00:44:39 +0000

    Deploying to gh-pages from @ kylekim-dev/kylekim-dev.github.io@766b8477618bb1c63ab4d31b7389bec487575490 🚀

--- _next/static/ls5APIKt91E3Pk012Viyx/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/253-dda470fc571b0fee.js","static/chunks/pages/index-38a4f1fffb5d9459.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],"/kr":["static/chunks/pages/kr-2e3896a5149094df.js"],sortedPages:["/","/_app","/_error","/kr"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
